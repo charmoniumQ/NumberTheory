a2a120702fba57c40ac8b70d816d6073ab68c214
charmoniumQ <sam@samgrayson.me>
2017-02-25 15:50:49 -0600

    editted runtime calculation

--- haskell/k-ary.hs
+++ haskell/k-ary.hs
@@ -21,7 +21,7 @@ k_gcd :: Int -> Int -> Int -> Int
 
 -- g(a, b, k) = h(a, k) + h(b, k)
 -- Computing the intersection is linear with both lists, probably much less than h(a, k),
--- therefore, I have omitted the time it takes to compute the intersection, max(a, b).
+-- therefore, I have omitted the time it takes to compute the intersection, a + b.
 k_gcd a b k = last $ isect (k_divisors a k) (k_divisors b k)
 
 -- Let the runtime of this function be h(n, k)
@@ -34,49 +34,42 @@ k_divisors_ n k = [d | d <- [0..n], k_divides d n k]
 
 -- f(d, n, k) = g(d, n-d, k-1) = g(d, n-d, k-1) = h(d, k-1) + h(n-d, k-1)
 -- = sum [f(da, d, k-1) | da <- [0..d]] + sum [f(db, n-d, k-1) | db <- [0..n-d]]
--- < sum [f(da, b, k-1) | da <- [0..b]] + sum [f(db, b, k-1) | db <- [0..b]] where b = max(d, n-d) (note, this is an upper-bound (I think it is relatively tight))
--- = 2 * sum [f(da, b, k-1) | da <- [0..b]] where b = max(d, n-d)
--- = 2 * 2 * sum [f(da, b, k-1) | da <- [0..b/2]] where b = max(d, n-d) by a symmetry argument
+-- < sum [f(da, b, k-1) | da <- [0..b]] + sum [f(db, b, k-1) | db <- [0..b]] where b = max(d, n-d)
+-- (note, this is an upper-bound (I think it is relatively tight))
+-- = 2 sum [f(da, b, k-1) | da <- [0..b]] where b = max(d, n-d)
+-- = 2 * 2 sum [f(da, b, k-1) | da <- [0..b/2]] where b = max(d, n-d) by a symmetry argument
 -- (Since f(a, b, k) = f(b, a, k), so each term except perhaps the middle one is double-counted)
-
 -- f(d, n, k) ~ j(max(d, n-d), k), to reduce the number of parameters
--- j(b, k) = 4 * sum [j(b - da, k-1) | da <- [0..b/2]]
--- 2*(b/2 * min [j(b - da, k-1) | da <- [0..b/2]]) <= j(b, k) <= 2*(b/2 * max [j(b - da, k-1) | da <- [0..b/2]])
---    b  *  j(b/2, k-1)                            <= j(b, k) <=    b  *  j(b, k-1)
+-- j(b, k) = 4 sum [j(b - da, k-1) | da <- [0..b/2]]
+-- 4 (b/2 min [j(b - da, k-1) | da <- [0..b/2]]) <= j(b, k) <= 4 (b/2 max [j(b - da, k-1) | da <- [0..b/2]])
+-- 2  b        j(b/2   , k-1)                    <= j(b, k) <= 2  b        j(b     , k-1)
+-- j(b, k) <= 2b j(b, k-1) = (2b)^2 j(b, k-2) = (2b)^3 j(b, k-3) = ... = (2b)^k j(b, 0)
 -- and j(b, 0) = 1
--- b^(log_2 k)<= j(b, k) <= b^k
-
--- computes the intersection of two sorted lists
-isect :: [Int] -> [Int] -> [Int]
-isect [] _ = []
-isect _ [] = []
-isect (x:xs) (y:ys) =
-  if x == y
-  then (isect xs ys) ++ [x]
-  else
-    if x > y
-    then isect (x:xs) (ys)
-    else isect (xs) (y:ys)
-
--- tests if two sorted lists are disjoint
--- disjoint a b = null $ isect a b
--- except it doesn't have to compute the WHOLE intersection
-disjoint :: [Int] -> [Int] -> Bool
-disjoint [] _ = False
-disjoint _ [] = False
-disjoint (x:xs) (y:ys) =
-  if x == y
-  then True
-  else
-    if x > y
-    then disjoint (x:xs) (ys)
-    else disjoint (xs) (y:ys)
+-- (2b)^(log_2 k) <= j(b, k) <= (2b)^k
+-- f(d, n, k) in BigOmega((2b)^(log_2 k)) and f(d, n, k) in BigOh((2b)^k)
+-- Done
 
 test :: (Int, Int) -> [[Int]]
 test (maxk, maxn) = [k_divisors n k | k <- [0..maxk], n <- [0..maxn]]
-
 
 main = defaultMain [bench "thing 1" $ nf test (100, 300)]
 -- ghc -O3 --make k-ary && ./k-ary
 -- min     avg     max   (4 samples)
 -- 843 ms  843 ms  843 ms
+
+-- Helper function:
+-- computes the intersection of two sorted lists
+-- Let the runtime of this isect xs ys be k(a+b) where a = length xs and b = length ys
+-- (I am anticipating that the function's runtime is related to the sum of a and b, not a more mathematically complex operation)
+isect :: [Int] -> [Int] -> [Int]
+isect [] _ = [] -- runs in O(1)
+isect _ [] = [] -- runs in O(1)
+isect (x:xs) (y:ys) =
+-- always takes off at least one head k(a+b) = 1 + k(a+b-1)
+  if x == y
+  then (isect xs ys) ++ [x] -- take off both heads
+  else
+    if x > y
+    then isect (x:xs) (ys) -- takes off ys head
+    else isect (xs) (y:ys) -- takes off xs head
+-- k(a+b) = a+b, my guess was correct
