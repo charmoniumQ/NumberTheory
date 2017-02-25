31f8d4df9c51a2fcf69ab824bdae836c7ed9ee68
charmoniumQ <sam@samgrayson.me>
2017-02-25 15:34:44 -0600

    added runtime computation

--- haskell/k-ary.hs
+++ haskell/k-ary.hs
@@ -1,38 +1,50 @@
 import Criterion.Main
 import Data.Function.Memoize
 
--- runs in f(d, n, k) time
+-- Let the runtime of this funciton be f(d, n, k)
 k_divides :: Int -> Int -> Int -> Bool
+
 -- f(d, n, 0) = 1
 k_divides d n 0 = d <= n
+
 -- f(d, n, k) = g(d, n - d, k - 1)
 k_divides d n k = d <= n && k_coprime d (n - d) (k - 1)
 
--- runs in g(a, b, k) time
+-- Runs in g(a, b, k) time
 k_coprime :: Int -> Int -> Int -> Bool
---k_coprime a b k = not $ disjoint (tail $ k_divisors a k) (tail $ k_divisors b k)
--- g(a, b, k) = g(a, b, k) in the worst case
+
+-- g(a, b, k) = g(a, b, k)
 k_coprime a b k = 0 == k_gcd a b k
 
--- runs in g(a, b, k) time
+-- Let the runtime of this function be g(a, b, k)
 k_gcd :: Int -> Int -> Int -> Int
+
 -- g(a, b, k) = h(a, k) + h(b, k)
+-- Computing the intersection is linear with both lists, probably much less than h(a, k),
+-- therefore, I have omitted the time it takes to compute the intersection, max(a, b).
 k_gcd a b k = last $ isect (k_divisors a k) (k_divisors b k)
 
--- runs in h(n, k) time
+-- Let the runtime of this function be h(n, k)
 k_divisors :: Int -> Int -> [Int]
 k_divisors = memoize2 k_divisors_
 k_divisors_ :: Int -> Int -> [Int]
+
 -- h(n, k) = sum [f(d, n, k) | d <- [0..n]]
 k_divisors_ n k = [d | d <- [0..n], k_divides d n k]
 
 -- f(d, n, k) = g(d, n-d, k-1) = g(d, n-d, k-1) = h(d, k-1) + h(n-d, k-1)
--- = (sum [f(da, d, k-1) | da <- [0..d]]) + (sum [f(db, n-d, k-1) | db <- [0..n-d]])
--- ~ sum [f(da, b, k-1) | da <- [0..b]] where b = max(d, n-d)
--- ~ 2 * (sum [f(da, b, k-1) | da <- [0..b/2]]) where b = max(d, n-d)
+-- = sum [f(da, d, k-1) | da <- [0..d]] + sum [f(db, n-d, k-1) | db <- [0..n-d]]
+-- < sum [f(da, b, k-1) | da <- [0..b]] + sum [f(db, b, k-1) | db <- [0..b]] where b = max(d, n-d) (note, this is an upper-bound (I think it is relatively tight))
+-- = 2 * sum [f(da, b, k-1) | da <- [0..b]] where b = max(d, n-d)
+-- = 2 * 2 * sum [f(da, b, k-1) | da <- [0..b/2]] where b = max(d, n-d) by a symmetry argument
+-- (Since f(a, b, k) = f(b, a, k), so each term except perhaps the middle one is double-counted)
 
--- Let j(b, k) = 2 * (sum [j(b - da, k-1) | da <- [0..b/2]])
--- 
+-- f(d, n, k) ~ j(max(d, n-d), k), to reduce the number of parameters
+-- j(b, k) = 4 * sum [j(b - da, k-1) | da <- [0..b/2]]
+-- 2*(b/2 * min [j(b - da, k-1) | da <- [0..b/2]]) <= j(b, k) <= 2*(b/2 * max [j(b - da, k-1) | da <- [0..b/2]])
+--    b  *  j(b/2, k-1)                            <= j(b, k) <=    b  *  j(b, k-1)
+-- and j(b, 0) = 1
+-- b^(log_2 k)<= j(b, k) <= b^k
 
 -- computes the intersection of two sorted lists
 isect :: [Int] -> [Int] -> [Int]
@@ -64,7 +76,7 @@ test :: (Int, Int) -> [[Int]]
 test (maxk, maxn) = [k_divisors n k | k <- [0..maxk], n <- [0..maxn]]
 
 
-main = defaultMain [bench "thing 1" $ nf test (500, 1500)]
+main = defaultMain [bench "thing 1" $ nf test (100, 300)]
 -- ghc -O3 --make k-ary && ./k-ary
 -- min     avg     max   (4 samples)
 -- 843 ms  843 ms  843 ms
