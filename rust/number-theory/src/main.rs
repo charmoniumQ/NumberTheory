36f18f59f0e4f795caee5df672fc898cef9bb244
Sam Grayson <sam@samgrayson.me>
2018-04-08 17:13:53 -0500

    added timing code

--- rust/number-theory/src/main.rs
+++ rust/number-theory/src/main.rs
@@ -7,9 +7,10 @@
 #[warn(dead_code)]
 extern crate error_chain;
 extern crate image;
-extern crate core;
 extern crate scarlet;
-//extern crate rayon;
+extern crate core;
+extern crate time;
+extern crate rayon;
 
 use std::fmt::Display;
 use std::path::Path;
@@ -17,40 +18,56 @@ mod afunc;
 mod util;
 
 fn main2(k: usize, size: usize) {
-    // TODO: mkdir
+    // TODO: rm -rf and mkdir output/
+
+    let mut start_time = time::now();
     let chartri = afunc::CharTri::kary(size);
-    println!("Chartri");
+    println!("Chartri {}", (time::now() - start_time).num_milliseconds());
 
-    // let mut mus = Vec::<Vec<i16>>::new();
-    // for i in 0..k {
-    //     println!("Image {}", i);
-    //     let div = chartri.afunc(i);
-    //     // div.draw_image(Path::new(&format!("output/tri/{}.png", i)));
-    //     mus.push(div.mu());
-    // }
-    // println!("Mus");
+    let mut mus = Vec::<Vec<i16>>::new();
+    let mut afunc = 0; let mut image = 0; let mut must = 0;
+    for i in 0..k {
 
-    // let mut rows = Vec::<Vec<Box<Display>>>::new();
+        start_time = time::now();
+        let div = chartri.afunc(i);
+        afunc += (time::now() - start_time).num_milliseconds();
 
-    // let mut first_row = Vec::<Box<Display>>::new();
-    // first_row.push(Box::new("k"));
-    // first_row.push(Box::new("mu(k)"));
-    // rows.push(first_row);
+        start_time = time::now();
+        div.draw_image(Path::new(&format!("output/tri/{}.png", i)));
+        image += (time::now() - start_time).num_milliseconds();
 
-    // for (i, mu) in mus.iter().enumerate() {
-    //     let mut row = Vec::<Box<Display>>::new();
-    //     row.push(Box::new(i));
-    //     for e in mu {
-    //         row.push(Box::new(e.clone()));
-    //     }
-    //     rows.push(row);
-    // }
-    // println!("Mus2");
-    // util::to_csv(rows, Path::new("output/mus.csv")).unwrap();
+        start_time = time::now();
+        mus.push(div.mu());
+        must += (time::now() - start_time).num_milliseconds();
+    }
+    println!("Afunc   {}", afunc);
+    println!("Image   {}", image);
+    println!("Must    {}", must);
 
+    let mut rows = Vec::<Vec<Box<Display>>>::new();
+
+    let mut first_row = Vec::<Box<Display>>::new();
+    first_row.push(Box::new("k"));
+    first_row.push(Box::new("mu(k)"));
+    rows.push(first_row);
+
+    start_time = time::now();
+    for (i, mu) in mus.iter().enumerate() {
+        let mut row = Vec::<Box<Display>>::new();
+        row.push(Box::new(i));
+        for e in mu {
+            row.push(Box::new(e.clone()));
+        }
+        rows.push(row);
+    }
+    util::to_csv(rows, Path::new("output/mus.csv")).unwrap();
+    println!("CSV     {}", (time::now() - start_time).num_milliseconds());
+
+    start_time = time::now();
     chartri.draw_image(Path::new("output/chartri.png"));
+    println!("Chartri {}", (time::now() - start_time).num_milliseconds());
 }
 
 fn main() {
-    main2(500, 500);
+    main2(300, 300);
 }
